dd18e750b50e529313c8a2f36296a13d11117eb0
yerasi.vishnuvardhan <yerasi.vishnuvardhanreddy@ivycomptech.com>
2025-05-15 08:53:46 +0530

    dsa anagram

--- Anagram/main.go
+++ Anagram/main.go
@@ -19,27 +19,51 @@ func main() {
 
 }
 
+// ---------------------------------------------------------------------------
+// func anagram(input, output []rune) bool {
+// 	if len(input) != len(output) {
+// 		return false
+// 	}
+
+// 	for i := 0; i < len(input); i++ {
+// 		for j := 0; j < len(output); j++ {
+// 			if input[i] == output[j] {
+// 				output[j] = 0
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return isAllZero(output)
+// }
+
+// func isAllZero(output []rune) bool {
+// 	for _, r := range output {
+// 		if r != 0 {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
+
+//-----------------------------------------------------------------------------------------
+
+//way2 --> using HASH TABLE
+
 func anagram(input, output []rune) bool {
 	if len(input) != len(output) {
 		return false
 	}
 
-	for i := 0; i < len(input); i++ {
-		for j := 0; j < len(output); j++ {
-			if input[i] == output[j] {
-				output[j] = 0
-				break
-			}
-		}
+	counts := make(map[rune]int)
+	for _, r := range input {
+		counts[r]++
 	}
-	return isAllZero(output)
-}
-
-func isAllZero(output []rune) bool {
 	for _, r := range output {
-		if r != 0 {
+		counts[r]--
+		if counts[r] < 0 {
 			return false
 		}
 	}
+
 	return true
 }
